b8abac8de11e90ee0eafb7f903b82dda18bd2de8
leedg <movenine@gmail.com>
2022-01-10 02:44:47 +0900

    d20220110v1.00

--- systray.py
+++ systray.py
@@ -1,12 +1,9 @@
 import os
 import sys
-
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from PySide6 import QtWidgets, QtGui
-
 import gui_hapEncoder
+from PyQt5 import QtWidgets, QtCore, QtGui
+
+#from PySide6 import QtWidgets, QtGui
 
 class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
     """
@@ -15,6 +12,7 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
     def __init__(self, icon, parent=None):
         QtWidgets.QSystemTrayIcon.__init__(self, icon, parent)
         self.setToolTip(f'CUDO Server Manager - v1.00')
+
         menu = QtWidgets.QMenu(parent)
         app_Hapcodec = menu.addAction("HAP 코덱변환")
         app_Hapcodec.setToolTip(f'HAP코덱변환(H.264 -> HAP)')
@@ -36,28 +34,24 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
         :return:
         """
         if reason == self.DoubleClick:
-            self.open_notepad()
+            self.open_hapEncoder()
         # if reason == self.Trigger:
         #     self.open_notepad()
 
     # --------------------- Open HapEncoder window ---------------------- #
     def open_hapEncoder(self):
-        try:
-            self.hapencoder_window = gui_hapEncoder.mainGUI(self)
-            self.hapencoder_window.show()
-        except (OSError, TypeError) as e:
-            QMessageBox.information(self, '오류', '유효하지 않는 요청입니다.')
-            print(e)
-            pass
+        self.hapencoder_window = gui_hapEncoder.mainGUI()
+        self.hapencoder_window.show()
 
-def main():
+def main(image):
     app = QtWidgets.QApplication(sys.argv)
     w = QtWidgets.QWidget()
-    tray_icon = SystemTrayIcon(QtGui.QIcon("icon.png"), w)
+    tray_icon = SystemTrayIcon(QtGui.QIcon(image), w)
     tray_icon.show()
-    tray_icon.showMessage('VFX Pipeline', 'Hello "Name of logged in ID')
+    tray_icon.showMessage('Cudo Communication', 'www.ailed.co.kr')
     sys.exit(app.exec())
 
 
 if __name__ == '__main__':
-    main()
+    icon = "icon.png"
+    main(icon)
