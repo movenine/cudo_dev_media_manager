24def2d477f4f218beed2230a3860988fa48e747
leedg <movenine@gmail.com>
2022-01-10 02:13:49 +0900

    d20220110v1.00

--- systray.py
+++ systray.py
@@ -1,6 +1,12 @@
 import os
 import sys
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 from PySide6 import QtWidgets, QtGui
+
+import gui_hapEncoder
 
 class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
     """
@@ -8,11 +14,12 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
     """
     def __init__(self, icon, parent=None):
         QtWidgets.QSystemTrayIcon.__init__(self, icon, parent)
-        self.setToolTip(f'VFX Pipeline Application Build - 3.2.56')
+        self.setToolTip(f'CUDO Server Manager - v1.00')
         menu = QtWidgets.QMenu(parent)
-        open_app = menu.addAction("HAP 코덱변환")
-        open_app.triggered.connect(self.open_hapEncoder)
-        open_app.setIcon(QtGui.QIcon("icon.png"))
+        app_Hapcodec = menu.addAction("HAP 코덱변환")
+        app_Hapcodec.setToolTip(f'HAP코덱변환(H.264 -> HAP)')
+        app_Hapcodec.triggered.connect(self.open_hapEncoder)
+        app_Hapcodec.setIcon(QtGui.QIcon("icon.png"))
 
         exit_ = menu.addAction("Exit")
         exit_.triggered.connect(lambda: sys.exit())
@@ -33,12 +40,15 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
         # if reason == self.Trigger:
         #     self.open_notepad()
 
+    # --------------------- Open HapEncoder window ---------------------- #
     def open_hapEncoder(self):
-        """
-        this function will open application
-        :return:
-        """
-        os.system('notepad')
+        try:
+            self.hapencoder_window = gui_hapEncoder.mainGUI(self)
+            self.hapencoder_window.show()
+        except (OSError, TypeError) as e:
+            QMessageBox.information(self, '오류', '유효하지 않는 요청입니다.')
+            print(e)
+            pass
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
